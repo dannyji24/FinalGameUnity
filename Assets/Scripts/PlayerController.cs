f244352faa46e7b809e427d76e4f6c5fd2170b61
NianLyu <lyun23@bolles.org>
2022-12-08 14:18:33 -0500

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private float zRange = 2.2f;
+    private bool moveOr = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            moveOr = false;
+        }
+        if (isOnGround)
+            moveOr = true;
+        if (transform.position.z > -zRange && Input.GetKeyDown(KeyCode.A) && moveOr)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 120);
+        }
+        if (transform.position.z < zRange && Input.GetKeyDown(KeyCode.D) && moveOr)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 120);
+        }
+
+    }
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+        else if (collision.gameObject.CompareTag("Barrel") || collision.gameObject.CompareTag("Barrel2")|| 
+            collision.gameObject.CompareTag("wall"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+        else if (collision.gameObject.CompareTag("gold"))
+        {
+            Debug.Log("+1");
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+       
+    }
+    */
+}
